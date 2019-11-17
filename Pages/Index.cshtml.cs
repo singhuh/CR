039420d82914806e281e079abc80b1106e62112a
Umesh Singh <umesmail@gmail.com>
2019-11-16 23:18:35 -0500

    Add project files.

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using QuickType;
+
+namespace CR.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            using (var webClient = new WebClient())
+            {
+                string jsonString = webClient.DownloadString("https://calendarific.com/api/v2/holidays?&api_key=3b2cc4a9fcb58a121947815135fca92694b0f63e&country=US&year=2019");
+                var holiday = Holiday.FromJson(jsonString);
+                ViewData["Holiday"] = holiday;
+            }
+        }
+    }
+}
